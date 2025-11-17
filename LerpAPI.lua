493c2617f50498981b9f701242311106c629783e
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-11-16 22:58:15 -0700

    Spring update
    - Lerps can now act like springs.
      - Bouncy :)

--- LerpAPI.lua
+++ LerpAPI.lua
@@ -8,7 +8,7 @@
 --         \ \__\ \ \_______\   \ \__\ \ \__\ \__\ \_______\
 --          \|__|  \|_______|    \|__|  \|__|\|__|\|_______|
 --
--- Version: 1.1.0
+-- Version: 1.2.0
 
 -- Create API
 local lerpAPI = {}
@@ -35,6 +35,7 @@ function lerpInternal:reset(pos)
 	self.currTick = pos
 	self.target   = pos
 	self.currPos  = pos
+	self.vel      = 0
 	
 	-- Return object
 	return self
@@ -42,17 +43,42 @@ function lerpInternal:reset(pos)
 end
 
 -- Create a lerp object
-function lerpAPI:new(speed, pos)
+function lerpAPI:new(pos, stiff, damp, mass)
 	
 	-- Create object
 	local obj = setmetatable({}, { __index = lerpInternal })
 	
-	-- Speed
-	obj.speed = speed
-	
-	-- Lerp variables
+	--[[
+		Lerp variables:
+		The initial variables the lerp uses to control it position, in tick and render
+	--]]
 	obj:reset(pos)
 	
+	--[[
+		Stiffness:
+		How fast the object moves towards its target (in percentage each tick)
+		0 means it will never approach the target
+		1 means it will reach the target within the tick
+	--]]
+	obj.stiff = stiff or 0.2
+	
+	--[[
+		Damping:
+		How much an object is allowed to bounce around its target
+		0 means it will never reach its target due to bouncing
+		1 means it wont bounce around the target
+	--]]
+	obj.damp = damp or 1
+	
+	--[[
+		Mass:
+		How long it takes for the object to change velocity
+		Cannot have a mass of 0, otherwise divide by 0 errors will occur
+		You can *still* do 0 by changing it in field, but ur asking for issues at that point
+	--]] 
+	if mass == 0 then error("\n\n§6Mass cannot be 0.\n§c", 2) end
+	obj.mass = mass or 1
+
 	-- Lerp enabled
 	obj.enabled = true
 	
@@ -72,8 +98,14 @@ events.TICK:register(function()
 			-- Reset ticks
 			obj.prevTick = obj.currTick
 			
+			-- Calc
+			local fSpring = -obj.stiff * (obj.currTick - obj.target)
+			local fDamp   = -obj.damp * obj.vel
+			local acc     = (fSpring + fDamp) / obj.mass
+			
 			-- Apply
-			obj.currTick = math.lerp(obj.currTick, obj.target, obj.speed)
+			obj.vel = obj.vel + acc
+			obj.currTick = obj.currTick + obj.vel
 			
 		end
 	end
